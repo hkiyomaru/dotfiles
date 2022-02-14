6aed9d64ccb70e5014375a4cbe5f1eabd2439c1b
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2022-02-15 01:42:08 +0900

    tweak

--- zsh/zshrc.d/plugin.zsh
+++ zsh/zshrc.d/plugin.zsh
@@ -6,7 +6,7 @@ zinit ice wait lucid
 zinit light zsh-users/zsh-completions
 
 # Fish-like fast/unobtrusive autosuggestions for zsh.
-zinit ice wait lucid
+zinit ice wait lucid atload"_zsh_autosuggest_start"
 zinit light zsh-users/zsh-autosuggestions
 
 # Fish shell-like syntax highlighting for Zsh.
