3f74be8f920f7e809699c08596d85721bb03c8c3
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2022-02-16 14:38:11 +0900

    tweak

--- install.sh
+++ install.sh
@@ -100,7 +100,7 @@ setup_shell() {
             Darwin)
                 # Use zsh installed by Homebrew
                 [[ -n "$(command -v brew)" ]] && zsh_path="$(brew --prefix)/bin/zsh" || zsh_path="$(which zsh)"
-                if ! [[grep "${zsh_path}" "/etc/shells"]]; then
+                if ! [[ grep "${zsh_path}" "/etc/shells" ]]; then
                     info "Adding ${zsh_path} to /etc/shells"
                     echo "$zsh_path" | sudo tee -a /etc/shells
                 fi
